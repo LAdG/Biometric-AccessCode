2f935e055a50776dfce9aa73e93782d35db6db58
Slasten Tikhon <slasten32@gmail.com>
2017-11-13 12:37:36 +1000

    Refactor tests

--- Tests/data_handler_test.py
+++ Tests/data_handler_test.py
@@ -4,22 +4,23 @@ Testing module
 
 import unittest
 import IncludeProject
-from DataHandler.Reader import read_file, list_to_matrix
+from DataHandler.Reader import read_matrix_file, list_to_matrix
 
 DATA_PATH = '/home/tikhon/Documents/Biometric-AccessCode/Tests/Data/'
 
 class TestReader(unittest.TestCase):
+    def setUp(self):
+        self.input_list = read_matrix_file(DATA_PATH + 'input_matrix.txt')
+
     def test_reading_file(self):
-        self.assertEqual(read_file(DATA_PATH + 'input.txt'), [1.1, 2.12, 3.123, 4.1234])
+        self.assertEqual(self.input_list, [1.1, 2.12, 3.123, 4.1234])
 
     def test_list_to_matrix(self):
-        input_list = read_file(DATA_PATH + 'input.txt')
-        self.assertEqual(list_to_matrix(input_list, 2, 2), [[1.1, 2.12], [3.123, 4.1234]])
+        self.assertEqual(list_to_matrix(self.input_list, 2, 2), [[1.1, 2.12], [3.123, 4.1234]])
 
     def test_list_to_matrix_exc(self):
-        input_list = read_file(DATA_PATH + 'input.txt')
-        list_len = len(input_list)
-        self.assertRaises(IndexError, list_to_matrix, input_list, list_len, list_len)
+        list_len = len(self.input_list)
+        self.assertRaises(IndexError, list_to_matrix, self.input_list, list_len, list_len)
 
 if __name__ == '__main__':
     unittest.main()
