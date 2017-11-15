7bb3213980b3d31ed7aaf2a6b466f47e22e23f83
Slasten Tikhon <slasten32@gmail.com>
2017-11-15 20:13:13 +1000

    Added pytest

--- Tests/data_handler_test.py
+++ Tests/data_handler_test.py
@@ -2,32 +2,29 @@
 Testing module
 """
 
-import unittest
+import pytest
 import IncludeProject
 from DataHandler.Reader import read_matrix_file, list_to_matrix, read_k1, read_k2
 
 DATA_PATH = '/home/tikhon/Documents/Biometric-AccessCode/Tests/Data/'
 
-class TestReader(unittest.TestCase):
-    def setUp(self):
+class TestReader:
+    def setup_class(self):
         self.input_list = read_matrix_file(DATA_PATH + 'input_matrix.txt')
 
     def test_reading_file(self):
-        self.assertEqual(self.input_list, [1.1, 2.12, 3.123, 4.1234])
+        assert(self.input_list == [1.1, 2.12, 3.123, 4.1234])
 
     def test_list_to_matrix(self):
-        self.assertEqual(list_to_matrix(self.input_list, 2, 2), [[1.1, 2.12], [3.123, 4.1234]])
+        assert(list_to_matrix(self.input_list, 2, 2) == [[1.1, 2.12], [3.123, 4.1234]])
 
     def test_list_to_matrix_exc(self):
         list_len = len(self.input_list)
-        self.assertRaises(IndexError, list_to_matrix, self.input_list, list_len, list_len)
+        with pytest.raises(IndexError):
+            list_to_matrix(self.input_list, list_len, list_len)
 
     def test_read_k1(self):
-        self.assertEqual(read_k1(DATA_PATH + 'input_k1.txt'), [1, -1, -1, 1, 1])
+        assert(read_k1(DATA_PATH + 'input_k1.txt') == [1, -1, -1, 1, 1])
 
     def test_read_k2(self):
-        self.assertEqual(read_k2(DATA_PATH + 'input_k2.txt'), [1, 0, 0, 1, 1])
-
-if __name__ == '__main__':
-    unittest.main()
-        
+        assert(read_k2(DATA_PATH + 'input_k2.txt') == [1, 0, 0, 1, 1])
