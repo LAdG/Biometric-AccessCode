0210f8a360155d7dee5aa01dd3665b4173fb1bdf
Slasten Tikhon <slasten32@gmail.com>
2018-04-27 00:30:11 +1000

    Added keys loaders

--- GUI/Slots.py
+++ GUI/Slots.py
@@ -1,10 +1,11 @@
 from PyQt5.QtWidgets import QFileDialog
 from .UI.ui_main import Ui_MainWindow
+import DataHandler.Reader as FileReader
 
 class MainWindowSlots(Ui_MainWindow):
     def __init__(self):
-        self.k1 = ''
-        self.k2 = ''
+        self.k1 = []
+        self.k2 = []
 
         self.ours_path = []
         self.ours = []
@@ -13,10 +14,10 @@ class MainWindowSlots(Ui_MainWindow):
         self.aliens = []
 
     def k1_load(self):
-        k1_path = self.__openFileNameDialog("k1 file (*.txt)")
+        self.k1 = self.__k_load(FileReader.read_k1)
 
     def k2_load(self):
-        k2_path = self.__openFileNameDialog("k2 file (*.txt)")
+        self.k2 = self.__k_load(FileReader.read_k2)
 
     def ours_load(self):
         ours_paths = self.__openFileNamesDialog("Matlab files (*.mat)")
@@ -26,6 +27,14 @@ class MainWindowSlots(Ui_MainWindow):
 
     def learn_network(self):
         pass
+
+    def __k_load(self, reader):
+        k_path = self.__openFileNameDialog("Key file (*.txt)")
+
+        if len(k_path) == 0:
+            return
+
+        return reader(k_path)
 
     def __openFileNameDialog(self, file_types):    
         options = QFileDialog.Options()
