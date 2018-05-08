d09380d88b6380bae1129e978938e73f21f9c445
Slasten Tikhon <slasten32@gmail.com>
2018-05-08 22:17:01 +1000

    Changed activation function for layer 1 and 2

--- Network/Neuron.py
+++ Network/Neuron.py
@@ -6,12 +6,8 @@ from math import exp
 
 def __activate1(x):
     # f = 2 / (1 + exp(x)) - 1
-    if x > 10:
-        f = -1
-    elif x < 10:
-        f = 1
 
-    return 1 if f >= 0 else -1
+    return 1 if x <= 0 else -1
 
 def neuron1_calc(v, mu_i, in1_row):
     x = mu_i
@@ -23,16 +19,12 @@ def neuron1_calc(v, mu_i, in1_row):
 
 def __activate2(x):
     # f = 2 / (1 + exp(x)) - 1
-    if x > 10:
-        f = -1
-    elif x < 10:
-        f = 1
 
-    return 1 if f >= 0 else 0
+    return 1 if x <= 0 else 0
 
 def neuron2_calc(w2, out1, in2_row):
-    print(out1)
     x = 0
+    
     for i in range(len(out1)):
         x += w2[i] * out1[i] * in2_row[i]
 
